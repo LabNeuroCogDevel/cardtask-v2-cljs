1207694ef3e31888c89be56bf97ca3600ebabb9b
Will Foran <willforan@gmail.com>
2021-08-18 16:47:05 -0400

    ✨ initial cljs <-> jsPsych

--- src/cardtask/core.cljs
+++ src/cardtask/core.cljs
@@ -1,2 +1,8 @@
 (ns cardtask.core)
+(enable-console-print!)
+(set! *warn-on-infer* true)
 (println "Hello world!")
+
+(set! js/trials (clj->js {:timeline [{:type "html-keyboard-response", :stimulus "hi"}]}))
+(js/jsPsych.init js/trials)
+
