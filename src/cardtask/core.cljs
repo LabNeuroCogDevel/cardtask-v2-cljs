c9a19515a6869611e84483f285ea3c2dd1456864
Will Foran <willforan@gmail.com>
2021-08-19 20:13:15 -0400

    ✨ working main loop

--- src/cardtask/core.cljs
+++ src/cardtask/core.cljs
@@ -1,7 +1,12 @@
 (ns cardtask.core
-  (:require [sablono.core :as sab :refer [html]]))
+  (:require
+   [cljsjs.react]
+   [cljsjs.react.dom]
+   [sablono.core :as sab :include-macros true]
+   [cljs.core.async :refer [<! chan sliding-buffer put! close! timeout]])
+  (:require-macros  [cljs.core.async.macros :refer [go-loop go]]))
 (enable-console-print!)
-(set! *warn-on-infer* true)
+(set! *warn-on-infer* false) ; maybe want to be true
 
 ;; 2 way choice from 3 "cards"
 ;; Left high prob win, Right low prob win, Middle always win (but require multiple pushes)
@@ -43,40 +48,108 @@
   ; [{:left 80 :middle 100 :right 20 :rep 3}]
   (for [b p3_and_rep] (mkblock b)))
 
-(defn singlecard "div for a card. mostly handled by css" [side]
-  (str "<div class='card " (name side) "' "
-       "onclick=simkey(" (first (side KEYS)) ")>"
-       (side CARDIMGS)
-       "</div>"))
 
-(defn showchoices "html for showing cards" [& {:keys [left middle right]}]
- (str (singlecard :left )
-      (singlecard :middle)
-      (singlecard :right)))
+(def event-states [:instruction :card :feeback])
+;; from flappy bird demo
+(def starting-state {:running? false
+                     :event-name :card ; :instruction :card :feedback
+                     :time-start 0
+                     :time-cur 0
+                     :time-delta 0
+                     :last-flip 0
+
+                     :cards-cur []
+                     :cards-list [] ; [ [{card1}, {card2}], [], ... ]
+
+                     :trial 0
+                     :responses [] ; {:side :rt :score :prob :keys [{:time :kp $k}]}
+                     :score 0})
+
+(defn state-fresh
+  "starting fresh. use starting-state but
+  * update all the time vars
+  * make a new sequence of cards
+  * set running? to true. TODO: maybe this happens elsewhere
+  NB. @STATE needs to be passed in so its updates are global?!"
+ [_ time]
+  (-> starting-state
+      (assoc :time-start time :time-cur time :last-flip time
+             :card-list (cardseq SCHEME)
+             :running? true)))
+
+(defn event-next?
+  "based on current event and time-dela, do we need to update?
+   returns updated state"
+  [state time]
+  (let [cur (:time-cur state)
+        last (:time-flip state)
+        card-list (:card-list state)]
+    (if (> (- cur last) CARDDUR)
+      state ;(assoc state :card-list (pop card-list))
+      state)))
+
+(defn time-update
+  "what to do at each timestep of AnimationFrame.
+   first call will be on unintialized time values"
+  [time state]
+ (-> state
+     (assoc :time-cur time :time-delta (- time (:start-time state)))
+     (event-next? time)
+     ;additional updates to state
+))
+
+(defonce STATE (atom starting-state))
+
+(defn run-loop [time]
+  "recursive loop to keep the task running"
+  (let [new-state (swap! STATE (partial time-update time))]
+  (when (:running? new-state)
+    (go (<! (timeout 30))
+            (.requestAnimationFrame js/window run-loop)))))
+
+(defn task-start
+  "will stop if not :running?"
+  []
+  (println "starting task!")
+  (println STATE)
+  (.requestAnimationFrame
+   js/window
+   (fn [time]
+     (reset! STATE (state-fresh @STATE time))
+     (run-loop time))))
+
+(defn keypress [e state]
+  (println e)
+  (.preventDefault e))
 
 
-(defn cardStart [trial] nil)
-(defn cardLoad [trial] nil)
-(defn cardFinish [^js/object data]
-  (set! (.-testme data) "xx")
-)
-
-(defn trialCard [stim]
-  (let [availKeys nil]
-    {:type "html-keyboard-response"
-     :stimulus stim
-     ;:post_trial_gap SCOREANIMATEDUR,
-     :choices availKeys
-     :trial_duration CARDDUR
-     :prompt ""
-     :response_ends_trial false
-     :on_start cardStart
-     :on_load cardLoad
-     :on_finish cardFinish}))
+(defn task-display
+  "html to render for display. updates for any change in display
+   TODO. different display for card, feedback, and instructions"
+  [{:keys [cards event score] :as state}]
+  (sab/html
+   [:div.board
+    {:onKeyPress (fn [e] (keypress e STATE))}
+    [:h1.score (str "SCORE: " score)]
+    [:h3.run "running? " (str (:running? state))]
+    ;[:h2.time "time: " (:time-cur state)]
+]))
 
 
-(defn runOne [trial] (js/jsPsych.init (clj->js {:timeline [trial]})))
-;; run!
-(def trials {:timeline [{:type "html-keyboard-response" :stimulus "hi"}, (trialCard "stim")]})
-(js/jsPsych.init (clj->js trials))
+(let [node (.getElementById js/document "task")]
+  (defn renderer
+    "what id to fill w/#task-display"
+    [full-state]
+    (.render js/ReactDOM (task-display full-state) node)))
 
+(defn world
+  "function for anything used to wrap state.
+   currelty doesn't do anything. could score. phone home. etc"
+  [state]
+  (-> state))
+
+(add-watch STATE :renderer (fn [_ _ _ n] (renderer (world n))))
+
+; force a change that starts rendering
+(reset! STATE @STATE)
+(task-start)
